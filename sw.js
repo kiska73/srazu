732a969a160d8d3ec2df909073f5af7ae2bd167b
kiska73 <55405548+kiska73@users.noreply.github.com>
2026-01-26 16:27:09 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,52 +1,27 @@
-const CACHE_NAME = 'srazu-v3';
-
-const CORE_ASSETS = [
+const CACHE_NAME = 'srazu-v1';
+const urlsToCache = [
   '/',
   '/index.html',
   '/style.css',
   '/app.js',
-  '/site.webmanifest',  // Aggiornato al nuovo manifest
   '/apple-touch-icon.png',
   '/favicon-16x16.png',
   '/favicon-32x32.png',
-  '/android-chrome-192x192.png',
-  '/android-chrome-512x512.png',
-  '/favicon.ico'
+  '/site.webmanifest'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache =>
-      Promise.all(
-        CORE_ASSETS.map(url =>
-          fetch(url)
-            .then(res => {
-              if (!res.ok) throw new Error(url + ' not cached');
-              return cache.put(url, res);
-            })
-            .catch(err => console.warn(err.message))
-        )
-      )
-    )
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(names =>
-      Promise.all(
-        names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n))
-      )
-    )
-  );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(res => {
-      return res || fetch(event.request).catch(() => caches.match('/index.html'));
-    })
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
