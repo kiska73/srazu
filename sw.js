bcafecbe82d019b93afdd1fdad0109e0f1d11c40
kiska73 <55405548+kiska73@users.noreply.github.com>
2026-03-06 10:05:29 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,14 @@
 const CACHE_NAME = 'srazu-v6';
-
-const CORE_ASSETS = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/app.js',
-  '/site.webmanifest',
-  '/apple-touch-icon.png',
-  '/favicon-32x32.png',
-  '/favicon-16x16.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    ))
-  );
-  self.clients.claim();
-});
-
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-      .catch(() => caches.match('/index.html'))
-  );
+  if (event.request.url.includes('app.js')) {
+    event.respondWith(
+      fetch(event.request).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then(c => c.put(event.request, copy));
+        return res;
+      }).catch(() => caches.match(event.request))
+    );
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(r => r || fetch(event.request)));
 });
