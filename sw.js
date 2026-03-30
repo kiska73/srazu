621fbca0a96df0f174fcddb12bd9ca4cd63501b6
kiska73 <55405548+kiska73@users.noreply.github.com>
2026-03-30 18:42:53 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ====================== SRAZU SERVICE WORKER v2 (MARZO 2026) ======================
-const CACHE_NAME = 'srazu-v21';   // ← cambiato da v1 a v2 per forzare aggiornamento
+const CACHE_NAME = 'srazu-v1';   // ← cambiato da v1 a v2 per forzare aggiornamento
 
 const CORE_ASSETS = [
   '/',
